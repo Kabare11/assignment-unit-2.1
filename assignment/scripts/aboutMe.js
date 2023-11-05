11ee51f4381771ff0f3092386dee214107430b16
Abdulahi Kabare <abdullahikabare@gmail.com>
2023-11-05 01:16:27 -0600

    created variablesand consoled the variables

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,14 +1,24 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = '';
+let firstName = 'Abdulahi';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
+let lastName = 'Kabare';
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
+
+let fullName = 'Abdulahi Kabare';
 //     (remember, you already have variables for this, can you use those?)
+
 //     Console log the value of `fullName`
 
+console.log(fullName);
+
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
+
+let luckyNumber = 11;
+
 //     Console log the value of `luckyNumber`
+console.log(luckyNumber)
 
 // 5 - Create a variable `introduction` and using the variables from above,
 //     give it the value of:
